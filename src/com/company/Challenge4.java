51c71cbd1bbe5a849cc8f37489c00fd4df99807d
scheerc23 <campbellscheer@icloud.com>
2020-06-18 13:42:35 -0500

    updated

--- src/com/company/Challenge4.java
+++ src/com/company/Challenge4.java
@@ -1,30 +1,24 @@
 package com.company;
 
 public class Challenge4 {
-    public static final double tax = 0.8;
-    public static void main(String[] args) { //main
-        //call all my functions
+
+    public static void main(String[] args) {
         int bill1 = 38;
         int bill2 = 40;
         int bill3 = 30;
+        int subtotal = bill1 + bill2 + bill3;
+        double tipPercent = 0.08;
+        double taxPercent = 0.15;
+        double taxes = subtotal * taxPercent;
+        double tip = subtotal * tipPercent;
+        double total = subtotal + taxes + tip;
 
+        System.out.println("Your subtotal is: " + subtotal);
+        System.out.println("Your tax is: " + taxes);
+        System.out.println("Your tip is: " + tip);
+        System.out.println("Your total is:");
+        System.out.print(subtotal + taxes + tip);
     }
-
-    // Calculate total owed, assuming 8% tax / 15% tip
-    public static void subtotal() {
-        System.out.print("bill1+bill2+bill3");
-    }
-
-    public static void tax() {
-        System.out.print("subtotal*tax");
-    }
-
-    public static void tip() {
-        System.out.print("subtotal*0.15");
-    }
-
-    public static void total() {
-        System.out.print("subtotal+tip+tax");
-    }
-
 }
+
+

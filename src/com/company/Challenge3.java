d0fae91d554ade2ad3e65356caed9e33ab4cd379
scheerc23 <campbellscheer@icloud.com>
2020-06-18 12:30:22 -0500

    updated

--- src/com/company/Challenge3.java
+++ src/com/company/Challenge3.java
@@ -3,7 +3,13 @@ package com.company;
 public class Challenge3 {
     public static final int SIZE = 5;
 
+
     public static void main(String[] args) {
+        Top();
+        Bottom();
+    }
+
+    public static void Top() {
         char letter = 65;
         for (int i = SIZE; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
@@ -11,10 +17,17 @@ public class Challenge3 {
             }
             System.out.println();
         }
+    }
+
+    public static void Bottom() {
+        char letter = 65;
         for (int i = 0; i <= SIZE; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print((char) (letter + j) + " ");
             }
+            System.out.println();
         }
     }
 }
+
+
